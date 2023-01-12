f2fecdaa7d618bbe5f1cdef08c84e8175edd2806
Tae Woo <t.wooo@kakao.com>
2023-01-12 19:54:02 +0900

    [FIX] 임시 commit

--- app/src/main/java/kr/co/pureum/views/MainActivity.kt
+++ app/src/main/java/kr/co/pureum/views/MainActivity.kt
@@ -1,19 +1,44 @@
 package kr.co.pureum.views
 
-import android.content.ContentValues.TAG
-import android.util.Log
-import android.view.View
-import com.bumptech.glide.Glide
-import com.kakao.sdk.auth.model.OAuthToken
-import com.kakao.sdk.common.model.ClientError
-import com.kakao.sdk.common.model.ClientErrorCause
-import com.kakao.sdk.user.UserApiClient
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import kr.co.pureum.R
 import kr.co.pureum.base.BaseActivity
 import kr.co.pureum.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun initView() {
+        initToolBar();
+        initBottomNavigation();
     }
 
+    private fun initToolBar() {
+        val toolbarBodyTemplate = binding.mainToolbar
+        setSupportActionBar(toolbarBodyTemplate)
+        supportActionBar?.setLogo(R.drawable.ic_pureum_logo)
+        supportActionBar?.title = ""
+    }
+
+    private var navController : NavController? = null
+    private fun initBottomNavigation() {
+        navController = supportFragmentManager.findFragmentById(R.id.main_frame)?.findNavController()
+
+        if (intent.hasExtra("screen")){
+            val inflater = navController?.navInflater
+            val graph = inflater?.inflate(R.navigation.main_navigation_graph)
+            graph?.setStartDestination(
+                when(intent.getIntExtra("screen", 0)){
+                    1 -> R.id.homeFragment
+                    2 -> R.id.chatFragment
+                    3 -> R.id.questFragment
+                    else -> R.id.profileFragment
+                }
+            )
+            graph?.let { navController?.graph = it }
+        }
+
+        navController?.let { binding.mainNavigationBar.setupWithNavController(it) }
+    }
 }
--- app/src/main/java/kr/co/pureum/views/profile/ProfileFragment.kt
+++ app/src/main/java/kr/co/pureum/views/profile/ProfileFragment.kt
@@ -1,60 +1,43 @@
 package kr.co.pureum.views.profile
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import kr.co.pureum.R
+import kr.co.pureum.base.BaseFragment
+import kr.co.pureum.databinding.FragmentProfileBinding
+import kr.co.pureum.views.MainActivity
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_profile) {
+    lateinit var mainActivity: MainActivity
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initToolbar()
+        initView()
+        initListener()
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private fun initToolbar() {
+        mainActivity = context as MainActivity
+        with(mainActivity) {
+            supportActionBar?.setDisplayUseLogoEnabled(true)
+            supportActionBar?.setDisplayHomeAsUpEnabled(false)
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    private fun initView() {
+        binding.profileTextView.text = "프로필 화면입니다."
+        Log.e("ScreenBuild", "ProfileFragment")
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun initListener() {
+        with(binding) {
+            profileButton.setOnClickListener {
+                val action = ProfileFragmentDirections.actionProfileFragmentToProfileTempFragment()
+                findNavController().navigate(action)
             }
+        }
     }
 }
