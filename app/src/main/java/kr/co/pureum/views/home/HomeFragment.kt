da62abc35aab79e84091cc8d07b6e3b415a92c71
Tae Woo <t.wooo@kakao.com>
2023-01-13 16:31:58 +0900

    [RES] 기본 폰트 색상 #1A1A1B 로 설정

--- app/src/main/java/kr/co/pureum/views/home/HomeFragment.kt
+++ app/src/main/java/kr/co/pureum/views/home/HomeFragment.kt
@@ -1,17 +1,21 @@
 package kr.co.pureum.views.home
 
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import androidx.navigation.fragment.findNavController
+import androidx.annotation.RequiresApi
 import kr.co.pureum.R
 import kr.co.pureum.base.BaseFragment
 import kr.co.pureum.databinding.FragmentHomeBinding
 import kr.co.pureum.views.MainActivity
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
-    lateinit var mainActivity: MainActivity
+    private lateinit var mainActivity: MainActivity
+    private lateinit var today: LocalDateTime
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initToolbar()
@@ -26,7 +30,10 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun initView() {
-        binding.homeDate.text = "홈 화면입니다."
+        today = LocalDateTime.now()
+
+        binding.homeDate.text = today.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일"))
     }
 }
