2088d82b2a8a8131c969a9834bf9160c81850f0e
JIYOUNG PARK <ziy913@naver.com>
2023-01-15 20:03:53 +0900

    [ADD] 스플래쉬 1,2,3 화면 구현

---
+++ app/src/main/java/kr/co/pureum/views/signup/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.pureum.views.signup
+
+import kr.co.pureum.R
+import kr.co.pureum.base.BaseActivity
+import kr.co.pureum.databinding.ActivitySignUpBinding
+
+class SignUpActivity : BaseActivity<ActivitySignUpBinding>(R.layout.activity_sign_up) {
+
+    override fun initView() {
+
+    }
+}
