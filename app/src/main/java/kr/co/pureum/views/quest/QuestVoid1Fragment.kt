a1468e74c476fa77f70e8c10f2ca389c49dc7194
muldaeng <alstnwkd990@naver.com>
2023-01-18 19:34:20 +0900

    [ADD] 문장 작성 전 화면 구현

--- app/src/main/java/kr/co/pureum/views/quest/QuestVoid1Fragment.kt
+++ app/src/main/java/kr/co/pureum/views/quest/QuestVoid1Fragment.kt
@@ -3,15 +3,20 @@ package kr.co.pureum.views.quest
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import kr.co.pureum.R
 import kr.co.pureum.base.BaseFragment
 import kr.co.pureum.databinding.FragmentQuestVoid1Binding
+import kr.co.pureum.views.MainActivity
 
 class QuestVoid1Fragment : BaseFragment<FragmentQuestVoid1Binding>(R.layout.fragment_quest_void1) {
+    lateinit var mainActivity : MainActivity
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initView()
+        initToolbar()
+        initClickListener()
     }
 
     private fun initView() {
@@ -23,6 +28,17 @@ class QuestVoid1Fragment : BaseFragment<FragmentQuestVoid1Binding>(R.layout.frag
         binding.questWritingSentenceBt.setOnClickListener() {
             val intent = Intent(activity, WriteSentence1Activity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun initToolbar() {
+        mainActivity = context as MainActivity
+        with(mainActivity) {
+            supportActionBar?.setDisplayUseLogoEnabled(false)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            binding.mainToolbar.setNavigationOnClickListener {
+                findNavController().navigateUp()
+            }
         }
     }
 
--- app/src/main/java/kr/co/pureum/views/quest/QuestWritingBeforeFragment.kt
+++ app/src/main/java/kr/co/pureum/views/quest/QuestWritingBeforeFragment.kt
@@ -2,40 +2,32 @@ package kr.co.pureum.views.quest
 
 import android.os.Bundle
 import android.view.View
-import androidx.recyclerview.widget.LinearLayoutManager
-import kr.co.domain.model.DataSentence
+import androidx.navigation.fragment.findNavController
 import kr.co.pureum.R
-import kr.co.pureum.adapter.quest.DataSentenceRVAdapter
-import kr.co.pureum.adapter.quest.DataTodaySentenceRVAdapter
 import kr.co.pureum.base.BaseFragment
 import kr.co.pureum.databinding.FragmentQuestWritingBeforeBinding
 
 class QuestWritingBeforeFragment : BaseFragment<FragmentQuestWritingBeforeBinding>(R.layout.fragment_quest_writing_before) {
 
-    private val dataSentenceList : ArrayList<DataSentence> = arrayListOf()
-    private val dataSentenceAdapter = DataTodaySentenceRVAdapter(dataSentenceList)
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initRecyclerView()
+        initView()
+        initListener()
     }
 
     private fun initView() {
-
+        binding.questTodayKeywordOneTv.text = "구현"
+        binding.questTodayKeywordTwoTv.text = "바보"
+        binding.questTodayKeywordThreeTv.text = "호구"
     }
 
-    private fun initRecyclerView() {
-        val managerRecyclerView = LinearLayoutManager(activity)
-        managerRecyclerView.reverseLayout = true
-        managerRecyclerView.stackFromEnd = true
-        binding.questWritingBeforeRv.layoutManager = managerRecyclerView
-        binding.questWritingBeforeRv.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-        binding.questWritingBeforeRv.adapter = dataSentenceAdapter
-
-        dataSentenceList.apply {
-            add(DataSentence("오늘의 키워드 1", "성실"))
-            add(DataSentence("오늘의 키워드 2", "바보"))
-            add(DataSentence("오늘의 키워드 3", "호구"))
+    private fun initListener() {
+        with(binding) {
+            questTodaySentenceOneCv.setOnClickListener {
+                val action = QuestClickFragmentDirections.actionQuestClickFragmentToQuestVoidFragment()
+                findNavController().navigate(action)
+            }
         }
     }
+
 }
