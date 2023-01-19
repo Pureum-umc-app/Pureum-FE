75f8658b3a86a058f37d4e87143e063d6a86f5ec
muldaeng <alstnwkd990@naver.com>
2023-01-19 21:46:39 +0900

    [ADD] 문장 작성 화면 구현 중

--- app/src/main/java/kr/co/pureum/views/quest/WriteSentence1Activity.kt
+++ app/src/main/java/kr/co/pureum/views/quest/WriteSentence1Activity.kt
@@ -1,7 +1,9 @@
 package kr.co.pureum.views.quest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import kr.co.pureum.R
 import kr.co.pureum.base.BaseActivity
@@ -9,12 +11,19 @@ import kr.co.pureum.databinding.ActivityWriteSentence1Binding
 import kr.co.pureum.views.MainActivity
 
 class WriteSentence1Activity : BaseActivity<ActivityWriteSentence1Binding>(R.layout.activity_write_sentence1) {
-    lateinit var mainActivity : MainActivity
 
     override fun initView() {
-
+        initToolbar()
     }
 
-
-
+    private fun initToolbar() {
+        val toolbarBodyTemplate = binding.mainToolbar
+        setSupportActionBar(toolbarBodyTemplate)
+        supportActionBar?.title = ""
+        supportActionBar?.setDisplayUseLogoEnabled(false)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.mainToolbar.setOnClickListener{
+            finish()
+        }
+    }
 }
