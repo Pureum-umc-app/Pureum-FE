409fdde84b107ee232b010956d1c679cec8eac8e
Kim Tae Woo <t.wooo@kakao.com>
2023-01-20 03:03:30 +0900

    [ADD] 휴대폰 사용 목표시간 설정 다이얼로그 구현중

--- app/src/main/java/kr/co/pureum/views/home/HomeFragment.kt
+++ app/src/main/java/kr/co/pureum/views/home/HomeFragment.kt
@@ -1,21 +1,27 @@
 package kr.co.pureum.views.home
 
-import android.app.usage.UsageStats
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.view.View
+import android.view.*
 import androidx.annotation.RequiresApi
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import kr.co.pureum.R
 import kr.co.pureum.adapter.home.RankingAdapter
 import kr.co.pureum.adapter.home.UsageTimeAdapter
 import kr.co.pureum.base.BaseFragment
+import kr.co.pureum.databinding.BottomSheetSetGoalTimeBinding
 import kr.co.pureum.databinding.FragmentHomeBinding
 import kr.co.pureum.views.MainActivity
 import java.time.LocalDate
+
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     private lateinit var mainActivity: MainActivity
@@ -33,8 +39,25 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     private fun initToolbar() {
         mainActivity = context as MainActivity
         with(mainActivity) {
-            supportActionBar?.setDisplayUseLogoEnabled(true)
-            supportActionBar?.setDisplayHomeAsUpEnabled(false)
+            supportActionBar!!.setDisplayUseLogoEnabled(true)
+            supportActionBar!!.setDisplayHomeAsUpEnabled(false)
+            addMenuProvider(object : MenuProvider {
+                override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                    // Add menu items here
+                    menuInflater.inflate(R.menu.menu_toolbar, menu)
+                }
+
+                override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                    // Handle the menu selection
+                    return when (menuItem.itemId) {
+                        R.id.toolbar_calender -> {
+                            // TODO: 월간 목표 달성 여부 확인 페이지로 이동
+                            true
+                        }
+                        else -> false
+                    }
+                }
+            }, viewLifecycleOwner, Lifecycle.State.RESUMED)
         }
     }
 
@@ -78,8 +101,28 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
             homeMoreButton.setOnClickListener {
                 // TODO: 랭킹 전체 화면으로 이동
             }
-            homeGoalTime.setOnClickListener {
-                // TODO: 목표 시간 설정 다이얼로그 표시
+            homeGoalTimeLayout.setOnClickListener {
+                val dialog = BottomSheetDialog(mainActivity)
+                val dialogBinding = BottomSheetSetGoalTimeBinding.inflate(LayoutInflater.from(requireContext()))
+                dialog.setContentView(dialogBinding.root)
+                with(dialogBinding) {
+                    var time = 0
+                    homeDialogExitButton.setOnClickListener {
+                        dialog.dismiss()
+                    }
+                    homeDialogDecreaseButton.setOnClickListener {
+                        if (time in 1..24) time -= 1
+                        hour = time
+                    }
+                    homeDialogIncreaseButton.setOnClickListener {
+                        if (time in 0..23) time += 1
+                        hour = time
+                    }
+                    homeDialogNextButton.setOnClickListener {
+                        dialog.dismiss()
+                    }
+                }
+                dialog.show()
             }
         }
     }
