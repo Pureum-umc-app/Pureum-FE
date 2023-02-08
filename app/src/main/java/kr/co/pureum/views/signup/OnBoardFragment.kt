c0efa56fad89aba6c5bda8115eeb1a2df29b27b4
TAE-COLA <t.wooo@kakao.com>
2023-02-08 14:18:30 +0900

    [ADD] 카카오 로그인 구현 및 strings.xml 충돌 오류 수정

--- app/src/main/java/kr/co/pureum/views/signup/OnBoardFragment.kt
+++ app/src/main/java/kr/co/pureum/views/signup/OnBoardFragment.kt
@@ -1,38 +1,7 @@
 package kr.co.pureum.views.signup
 
-import android.content.Intent
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
 import kr.co.pureum.R
 import kr.co.pureum.base.BaseFragment
 import kr.co.pureum.databinding.FragmentOnBoardBinding
 
-
-class OnBoardFragment : BaseFragment<FragmentOnBoardBinding>(R.layout.fragment_on_board) {
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        initView()
-        initListener()
-    }
-
-    private fun initView() {
-//        binding.profileTextView.text = "프로필 화면입니다."
-//        Log.e("ScreenBuild", "ProfileFragment")
-    }
-
-    private fun initListener() {
-        with(binding) {
-//            profileButton.setOnClickListener {
-//                val action = kr.co.pureum.views.profile.ProfileFragmentDirections.actionProfileFragmentToProfileTempFragment()
-//                findNavController().navigate(action)
-//            }
-//
-//            profileOnboardButton.setOnClickListener {
-//                val intent = Intent(activity, kr.co.pureum.views.signup.OnBoardActivity::class.java)
-//                startActivity(intent)
-//            }
-        }
-    }
-}
+class OnBoardFragment : BaseFragment<FragmentOnBoardBinding>(R.layout.fragment_on_board) {}
