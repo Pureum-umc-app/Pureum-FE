febb4564ac02de95a3ece5250682b8fc5a616676
TAE-COLA <t.wooo@kakao.com>
2023-02-10 10:46:53 +0900

    [ADD] 약관 동의 레이아웃 수정 및 로직 수정

--- app/src/main/java/kr/co/pureum/views/signup/OnBoardFourFragment.kt
+++ app/src/main/java/kr/co/pureum/views/signup/OnBoardFourFragment.kt
@@ -6,10 +6,12 @@ import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import androidx.fragment.app.viewModels
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.ClientError
@@ -18,6 +20,8 @@ import com.kakao.sdk.user.UserApiClient
 import dagger.hilt.android.AndroidEntryPoint
 import kr.co.pureum.R
 import kr.co.pureum.base.BaseFragment
+import kr.co.pureum.databinding.BottomSheetClauseBinding
+import kr.co.pureum.databinding.BottomSheetSetGoalTimeBinding
 import kr.co.pureum.databinding.FragmentOnBoardFourBinding
 import kr.co.pureum.di.PureumApplication
 import kr.co.pureum.views.MainActivity
@@ -49,7 +53,7 @@ class OnBoardFourFragment : BaseFragment<FragmentOnBoardFourBinding>(R.layout.fr
     private fun observe() {
         viewModel.userTokenLiveData.observe(viewLifecycleOwner) {
             when (it) {
-                "error" -> bottomSheetOpen()
+                "error" -> openClauseBottomSheet()
                 else -> {
                     PureumApplication.spfManager.setUserToken(it)
                     val intent = Intent(requireContext(), MainActivity::class.java).apply {
@@ -142,134 +146,48 @@ class OnBoardFourFragment : BaseFragment<FragmentOnBoardFourBinding>(R.layout.fr
         }
     }
 
-    private fun bottomSheetOpen() {
-        var flag : Int = 0
-        var service : Int = 0
-        var info : Int = 0
+    private fun openClauseBottomSheet() {
+        val dialog = BottomSheetDialog(requireContext(), R.style.BottomSheetDialogTheme)
+        val dialogBinding =
+            BottomSheetClauseBinding.inflate(LayoutInflater.from(requireContext()))
+        dialog.setContentView(dialogBinding.root)
+        with(dialogBinding) {
+            clauseNextButton.isEnabled = false
 
-        // bottomSheetDialog 객체 생성
-        val bottomSheetDialog = activity?.let {
-            BottomSheetDialog(
-                it, R.style.BottomSheetDialogTheme
-            )
-        }
+            allAgreeCheckBox.setOnClickListener {
+                serviceAgreeCheckBox.isChecked = allAgreeCheckBox.isChecked
+                personalInfoAgreeCheckBox.isChecked = allAgreeCheckBox.isChecked
+                clauseNextButton.isEnabled = allAgreeCheckBox.isChecked
+            }
 
-        // layout_bottom_sheet를 뷰 객체로 생성
-        val bottomSheetView = layoutInflater.inflate(R.layout.sign_up_bottom_sheet, null)
+            serviceAgreeTextView.setOnClickListener {
+                val intent = Intent(requireContext(), SignUpClauseActivity::class.java)
+                startActivity(intent)
+                requireActivity().overridePendingTransition(R.anim.rightin_activity,R.anim.not_move_activity)
+            }
+            serviceAgreeCheckBox.setOnClickListener {
+                allAgreeCheckBox.isChecked = serviceAgreeCheckBox.isChecked && personalInfoAgreeCheckBox.isChecked
+                clauseNextButton.isEnabled = allAgreeCheckBox.isChecked
+            }
 
-        bottomSheetView.setBackgroundColor(Color.parseColor("#ffffff"))
-        bottomSheetView.findViewById<View>(R.id.signup_agree_next_bt).setOnClickListener {
+            personalInfoAgreeTextView.setOnClickListener {
+                val intent = Intent(requireContext(), SignUpClauseActivity::class.java)
+                startActivity(intent)
+                requireActivity().overridePendingTransition(R.anim.rightin_activity,R.anim.not_move_activity)
+            }
+            personalInfoAgreeCheckBox.setOnClickListener {
+                allAgreeCheckBox.isChecked = serviceAgreeCheckBox.isChecked && personalInfoAgreeCheckBox.isChecked
+                clauseNextButton.isEnabled = allAgreeCheckBox.isChecked
+            }
 
-            if(flag==1 || (service==1)&&(info==1)){
-                bottomSheetDialog?.dismiss()
-                val intent = Intent(activity?.applicationContext, SignUpProfileActivity::class.java)
+            clauseNextButton.setOnClickListener {
+                val intent = Intent(requireContext(), SignUpProfileActivity::class.java)
                 startActivity(intent)
                 requireActivity().finish()
-                activity?.overridePendingTransition(R.anim.rightin_activity,R.anim.not_move_activity)
-
-//                finish()
-//                overridePendingTransition(R.anim.not_move_activity,R.anim.rightout_activity)
+                requireActivity().overridePendingTransition(R.anim.rightin_activity,R.anim.not_move_activity)
+                dialog.dismiss()
             }
         }
-
-        // 약관 자세히 보기
-        bottomSheetView.findViewById<View>(R.id.service_agree_tv).setOnClickListener {
-            val intent = Intent(activity?.applicationContext , SignUpClauseActivity::class.java)
-            startActivity(intent)
-            activity?.overridePendingTransition(R.anim.rightin_activity,R.anim.not_move_activity)
-        }
-        bottomSheetView.findViewById<View>(R.id.my_info_agree_tv).setOnClickListener {
-            val intent = Intent(activity?.applicationContext, SignUpInfoActivity::class.java)
-            startActivity(intent)
-            activity?.overridePendingTransition(R.anim.rightin_activity,R.anim.not_move_activity)
-        }
-
-        val nextBt = bottomSheetView.findViewById<View>(R.id.signup_agree_next_bt) as Button
-        val allButton = bottomSheetView.findViewById<View>(R.id.signup_all_agree_ib) as? ImageView
-        val serviceBt = bottomSheetView.findViewById<View>(R.id.service_agree_ib) as ImageView
-        val myInfoBt = bottomSheetView.findViewById<View>(R.id.my_info_agree_ib) as ImageView
-
-        // 모두 동의를 눌렀을 때
-        bottomSheetView.findViewById<View>(R.id.signup_all_agree_ib).setOnClickListener {
-
-            if(flag==0){
-
-                allButton?.setImageResource(R.drawable.ic_signup_fill)
-                serviceBt.setImageResource(R.drawable.ic_signup_fill)
-                myInfoBt.setImageResource(R.drawable.ic_signup_fill)
-                flag = 1
-                service = 1
-                info = 1
-
-                nextBt.backgroundTintList = ColorStateList.valueOf(Color.rgb(133,181,255))
-                nextBt.setTextColor(Color.parseColor("#FFFFFF"))
-
-            } else{
-
-                allButton?.setImageResource(R.drawable.ic_signup_no_fill)
-                serviceBt.setImageResource(R.drawable.ic_signup_no_fill)
-                myInfoBt.setImageResource(R.drawable.ic_signup_no_fill)
-
-                flag = 0
-                service = 0
-                info = 0
-
-                nextBt.backgroundTintList = ColorStateList.valueOf(Color.rgb(216,236,255))
-                nextBt.setTextColor(Color.parseColor("#6E6D73"))
-            }
-        }
-
-        // 서비스만 동의
-        bottomSheetView.findViewById<View>(R.id.service_agree_ib).setOnClickListener {
-            if(service==0){
-                serviceBt.setImageResource(R.drawable.ic_signup_fill)
-                service = 1
-
-                if(flag==1 || (service==1 && info==1)){
-                    allButton?.setImageResource(R.drawable.ic_signup_fill)
-                    nextBt.backgroundTintList = ColorStateList.valueOf(Color.rgb(133,181,255))
-                    flag = 1
-                }
-            }
-            else{
-                serviceBt.setImageResource(R.drawable.ic_signup_no_fill)
-                service = 0
-
-                allButton?.setImageResource(R.drawable.ic_signup_no_fill)
-                flag = 0
-                nextBt.backgroundTintList = ColorStateList.valueOf(Color.rgb(216,236,255))
-                nextBt.setTextColor(Color.parseColor("#6E6D73"))
-
-            }
-        }
-
-        // 개인정보만 동의
-        bottomSheetView.findViewById<View>(R.id.my_info_agree_ib).setOnClickListener {
-            if(info==0){
-                myInfoBt.setImageResource(R.drawable.ic_signup_fill)
-                info = 1
-
-                if(flag==1 || (service==1 && info==1)){
-                    allButton?.setImageResource(R.drawable.ic_signup_fill)
-                    nextBt.backgroundTintList = ColorStateList.valueOf(Color.rgb(133,181,255))
-                    flag = 1
-                }
-            }
-            else{
-                myInfoBt.setImageResource(R.drawable.ic_signup_no_fill)
-                info = 0
-
-                allButton?.setImageResource(R.drawable.ic_signup_no_fill)
-                flag = 0
-                nextBt.backgroundTintList = ColorStateList.valueOf(Color.rgb(216,236,255))
-                nextBt.setTextColor(Color.parseColor("#6E6D73"))
-            }
-        }
-
-        // bottomSheetDialog 뷰 생성
-        bottomSheetDialog?.setContentView(bottomSheetView)
-
-        // bottomSheetDialog 호출
-        bottomSheetDialog?.show()
+        dialog.show()
     }
 }
