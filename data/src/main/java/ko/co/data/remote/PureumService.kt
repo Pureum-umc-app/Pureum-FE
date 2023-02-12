6d84b0be3cae9b9a29168eff33906d2fe32a52e0
muldaeng <alstnwkd990@naver.com>
2023-02-12 12:25:24 +0900

    [REF] sentenceComplete 반환 값 수정

--- data/src/main/java/ko/co/data/remote/PureumService.kt
+++ data/src/main/java/ko/co/data/remote/PureumService.kt
@@ -1,5 +1,6 @@
 package ko.co.data.remote
 
+import kr.co.domain.model.SentenceCompleteResponse
 import kr.co.domain.model.SentencesIncompleteResponse
 import kr.co.domain.model.SentencesListResponse
 import retrofit2.http.GET
@@ -11,7 +12,7 @@ interface PureumService {
     @GET("/sentences/incomplete/{userId}")
     suspend fun sentencesIncomplete(@Path("userId")userId: Int) : SentencesIncompleteResponse
     @GET("/sentences/complete/{userId}")
-    suspend fun sentencesComplete(@Path("userId")userId: Int) : SentencesIncompleteResponse
+    suspend fun sentencesComplete(@Path("userId")userId: Int) : SentenceCompleteResponse
     @GET("/sentences/{userId}")
     suspend fun sentencesList(
         @Query("limit")limit: Int,
